3dfbec00632a0cd1c5d5776c4afc880adc9c167e
ReynandrielPT <162027180+ReynandrielPT@users.noreply.github.com>
2024-12-11 16:17:44 +0700

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,16 +1,21 @@
 <?php
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'airline_ticketing');
-
 function getDBConnection()
 {
+    $dbUrl = getenv('postgres://u83pkdmj3viohg:p46ccb1af85a6a102328ef91fe3a46fc886bc21ae91b9df3cb70948f84e80c991@c8lj070d5ubs83.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d30mo4o64b32la');
+
+    $db = parse_url($dbUrl);
+
+    $host = $db['host'];
+    $port = $db['port'];
+    $user = $db['user'];
+    $password = $db['pass'];
+    $dbname = ltrim($db['path'], '/');
+
     try {
         $conn = new PDO(
-            "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME,
-            DB_USER,
-            DB_PASS,
+            "pgsql:host=$host;port=$port;dbname=$dbname",
+            $user,
+            $password,
             [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
         );
         return $conn;
