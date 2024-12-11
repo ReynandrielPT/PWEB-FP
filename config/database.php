be629251d875a645976bad188f76a21fa4ac5c8e
ReynandrielPT <162027180+ReynandrielPT@users.noreply.github.com>
2024-12-11 22:53:05 +0700

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,16 +1,16 @@
 <?php
 function getDBConnection()
 {
-    $dbUrl = getenv('postgres://u83pkdmj3viohg:p46ccb1af85a6a102328ef91fe3a46fc886bc21ae91b9df3cb70948f84e80c991@c8lj070d5ubs83.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d30mo4o64b32la');
-
-    $db = parse_url($dbUrl);
-
-    $host = $db['host'];
-    $port = $db['port'];
-    $user = $db['user'];
-    $password = $db['pass'];
-    $dbname = ltrim($db['path'], '/');
-
+$db = parse_url(getenv(postgres://u83pkdmj3viohg:p46ccb1af85a6a102328ef91fe3a46fc886bc21ae91b9df3cb70948f84e80c991@c8lj070d5ubs83.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d30mo4o64b32la
+));
+$conn_string = sprintf(
+    "host=%s port=%s dbname=%s user=%s password=%s sslmode=require",
+    $db['host'], 
+    $db['port'], 
+    ltrim($db['path'], '/'), 
+    $db['user'], 
+    $db['pass']
+);
     try {
         $conn = new PDO(
             "pgsql:host=$host;port=$port;dbname=$dbname",
